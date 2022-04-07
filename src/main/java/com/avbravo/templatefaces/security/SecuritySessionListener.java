e134dca22e75032155f9548a911983771aa5370b
avbravo <avbravo@gmail.com>
2022-04-07 15:08:21 -0500

    control de sesion expirada

--- src/main/java/com/avbravo/templatefaces/security/SecuritySessionListener.java
+++ src/main/java/com/avbravo/templatefaces/security/SecuritySessionListener.java
@@ -20,28 +20,29 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
  *
  * @author avbravo
  */
-public class SecuritySessionListener implements HttpSessionListener, Serializable {
-private Integer sessionTimeout=4;
-    @Override
-    public void sessionCreated(HttpSessionEvent se) {
-        System.out.println("********************************************************************");
-        System.out.println("Session created : " + se.getSession().getId() + " at " + new Date());
-        System.out.println("********************************************************************");
-        /**
-         * Establece el periodo de inactivad para cerrar la sesion.
-         */
-        HttpSession session = se.getSession();
-session.setMaxInactiveInterval(sessionTimeout*60);
-        System.out.println("----->>>> seundos "+(sessionTimeout*60));
-        System.out.println("---Estableciendo periodo de inactividad.....");
-    }
-
-    @Override
-    public void sessionDestroyed(HttpSessionEvent se) {
-        HttpSession session = se.getSession();
-        
-        System.out.println("********************************************************************");
-        System.out.println("session destroyed :" + session.getId() + " Logging out user... at  " + new Date());
-        System.out.println("********************************************************************");
-    }
+public class SecuritySessionListener implements Serializable {
+//public class SecuritySessionListener implements HttpSessionListener, Serializable {
+//private Integer sessionTimeout=4;
+//    @Override
+//    public void sessionCreated(HttpSessionEvent se) {
+//        System.out.println("********************************************************************");
+//        System.out.println("Session created : " + se.getSession().getId() + " at " + new Date());
+//        System.out.println("********************************************************************");
+//        /**
+//         * Establece el periodo de inactivad para cerrar la sesion.
+//         */
+//        HttpSession session = se.getSession();
+//session.setMaxInactiveInterval(sessionTimeout*60);
+//        System.out.println("----->>>> seundos "+(sessionTimeout*60));
+//        System.out.println("---Estableciendo periodo de inactividad.....");
+//    }
+//
+//    @Override
+//    public void sessionDestroyed(HttpSessionEvent se) {
+//        HttpSession session = se.getSession();
+//        
+//        System.out.println("********************************************************************");
+//        System.out.println("session destroyed :" + session.getId() + " Logging out user... at  " + new Date());
+//        System.out.println("********************************************************************");
+//    }
 }
